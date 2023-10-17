e26d96df85758719605a7089fedc2be8877a385a
Ubuntu <ubuntu@ip-172-31-6-103.ec2.internal>
2023-10-17 16:43:53 +0000

    update changes

--- js/app.js
+++ js/app.js
@@ -23,10 +23,18 @@
     // subscribe to the changes via Pusher
     var pusher = new Pusher('6780742e5e8b3e07326f', { cluster: 'us2' });
     var channel = pusher.subscribe(id);
-    var presenceChannel = pusher.subscribe("presence-quickstart");
+    var presenceChannel = pusher.subscribe("presence-quickstart" + id);
 
 
-
+    function addMemberToUserList(memberId) {
+      userEl = document.createElement("div");
+      userEl.id = "user_" + memberId;
+      userEl.innerText = memberId;
+      userEl.style.backgroundColor =
+        "hsl(" + (hashCode(memberId) % 360) + ",70%,60%)";
+      document.getElementById("user_list").appendChild(userEl);
+    }
+    
     presenceChannel.bind("pusher:subscription_succeeded", () =>
       presenceChannel.members.each((member) => addMemberToUserList(member.id))
     );
@@ -90,16 +98,7 @@
     return 'private-' + Math.random().toString(36).substr(2, 9);
   }
 
-  function addMemberToUserList(memberId) {
 
-
-    userEl = document.createElement("div");
-    userEl.id = memberId;
-    userEl.innerText = memberId;
-    document.getElementById("user_list").appendChild(userEl);
-
-    console.log = memberId.name;
-  }
   // function to get a query param's value
   function getUrlParameter(name) {
     name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
